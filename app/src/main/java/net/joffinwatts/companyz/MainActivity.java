93492b6c6a7ec4294686138f7e8056c1aca6f81e
JoffinWatts <platinum2112@gmail.com>
2020-02-27 13:10:41 -0500

    test comment

--- app/src/main/java/net/joffinwatts/companyz/MainActivity.java
+++ app/src/main/java/net/joffinwatts/companyz/MainActivity.java
@@ -3,7 +3,7 @@ package net.joffinwatts.companyz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//this is a comment
 public class MainActivity extends AppCompatActivity {
 
     @Override
